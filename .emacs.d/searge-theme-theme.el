502efbf9e8fd2ee0ad495d2c21a3b87ecbb5c0d9
Searge <ashikcygan@gmail.com>
2019-05-15 13:48:34 +0300

    Whole new settings

--- .emacs.d/searge-theme-theme.el
+++
@@ -1,8 +0,0 @@
-(deftheme searge-theme
-  "Created 2018-06-10.")
-
-(custom-theme-set-variables
- 'searge-theme
- '(package-selected-packages (quote (use-package spacemacs-theme ergoemacs-mode))))
-
-(provide-theme 'searge-theme)
